42135f748d24dbaaa9ef936d4c29091fd035fc21
kkidgamer <129827158+kkidgamer@users.noreply.github.com>
2025-08-01 08:58:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,6 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log('Connected to MongoDB'))
 .catch((err) => console.log("MongoDB connection error:", err));
 
-const PORT=3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
